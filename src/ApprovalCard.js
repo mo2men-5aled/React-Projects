433d2869deb45a91c9f523082dd9bfa458f84d95
momen <momenk208@gmail.com>
2022-08-11 15:12:54 +0200

    finished

--- src/ApprovalCard.js
+++ src/ApprovalCard.js
@@ -5,7 +5,7 @@ const ApprovalCard = (prop) => {
     <div className="ui card">
       <div className="content">{prop.children}</div>
       <div className="extra content">
-        <div className="ui twi buttons">
+        <div className="ui two buttons" style={{ width: "100%" }}>
           <div className="ui basic green button">Approve</div>
           <div className="ui basic red button">Reject</div>
         </div>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { faker } from "@faker-js/faker";
 import ApprovalCard from "./ApprovalCard";
-
 import CommentDetail from "./CommentDetail";
 const App = () => {
   return (
