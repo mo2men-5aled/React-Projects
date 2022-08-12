b87608d019e57eeb6595443fc2fddd2579c59431
momen <momenk208@gmail.com>
2022-08-12 22:43:42 +0200

    using class component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import DisplaySeason from "./DisplaySeason";
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (postion) => console.log(postion),
-    (err) => console.log(err)
-  );
-  return <div></div>;
-};
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (postion) => console.log(postion),
+      (err) => console.log(err)
+    );
+    return <div> Latitude :</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
