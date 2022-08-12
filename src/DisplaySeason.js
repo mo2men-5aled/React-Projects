4ce978a128c11e9dc0a96bb96dd1f19b4702d701
momen <momenk208@gmail.com>
2022-08-12 22:15:22 +0200

    commit

---
+++ src/DisplaySeason.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const DisplaySeason = () => {
+  return <div>Season</div>;
+};
+
+export default DisplaySeason;
--- src/index.js
+++ src/index.js
@@ -1,48 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { faker } from "@faker-js/faker";
-import ApprovalCard from "./ApprovalCard";
-import CommentDetail from "./CommentDetail";
+import DisplaySeason from "./DisplaySeason";
 const App = () => {
-  return (
-    <div className="ui container comments">
-      <ApprovalCard>
-        <CommentDetail
-          author="Ali"
-          timeAgo="Today at 6:00PM"
-          Comment="yeah, that's very good"
-          image={faker.image.avatar()}
-        />
-      </ApprovalCard>
-
-      <ApprovalCard>
-        <CommentDetail
-          author="Ahmed"
-          timeAgo="yasterday at 7:00PM"
-          Comment="nice to see you my friend"
-          image={faker.image.avatar()}
-        />
-      </ApprovalCard>
-
-      <ApprovalCard>
-        <CommentDetail
-          author="Hazem"
-          timeAgo="Thursday at 10:00PM"
-          Comment="oh my god it is yoiu again"
-          image={faker.image.avatar()}
-        />
-      </ApprovalCard>
-
-      <ApprovalCard>
-        <CommentDetail
-          author="Hassan"
-          timeAgo="Wed at 5:00PM"
-          Comment="omaywa mo shindayiro"
-          image={faker.image.avatar()}
-        />
-      </ApprovalCard>
-    </div>
+  window.navigator.geolocation.getCurrentPosition(
+    (postion) => console.log(postion),
+    (err) => console.log(err)
   );
+  return <div></div>;
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
