9de37556ad529c49c48fb67dd04afc9a5dedb3e3
momen <momenk208@gmail.com>
2022-08-13 21:28:02 +0200

    print latitude and longitude

--- src/index.js
+++ src/index.js
@@ -2,12 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import DisplaySeason from "./DisplaySeason";
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { lat: null };
+  }
   render() {
     window.navigator.geolocation.getCurrentPosition(
-      (postion) => console.log(postion),
+      (postion) =>
+        this.setState({
+          lat: postion.coords.latitude,
+          long: postion.coords.longitude,
+        }),
       (err) => console.log(err)
     );
-    return <div> Latitude :</div>;
+    return (
+      <div>
+        <div>Latitude : {this.state.lat}</div>
+        <div>longitude: {this.state.long}</div>
+      </div>
+    );
   }
 }
 
