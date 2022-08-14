8f414cc46f2d23067108de2c2caf5dc954e62469
momen <momenk208@gmail.com>
2022-08-15 00:27:21 +0200

    added a pattern for season

--- src/DisplaySeason.js
+++ src/DisplaySeason.js
@@ -1,6 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
-
+const seasonConfig = {
+  summer: { text: "let's hit hte beach", icon: "sun" },
+  winter: { text: "Burr, it is frozen", icon: "snowflake" },
+};
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     return lat > 0 ? "summer" : "winter";
@@ -10,14 +12,11 @@ const getSeason = (lat, month) => {
 };
 const DisplaySeason = (prop) => {
   const Season = getSeason(prop.lat, new Date().getMonth());
-  const icon = Season === "winter" ? "snowflake" : "sun";
-  console.log(Season);
+  const { text, icon } = seasonConfig[Season];
   return (
     <div>
       <i className={`${icon} icon`} />
-      <h1>
-        {Season === "winter" ? "Burr, it is frozen" : "Lets hit the beach"}
-      </h1>
+      <h1>{text}</h1>
       <i className={`${icon} icon`} />
     </div>
   );
