f8e9cb4167b292650f3361b261882ae195f6a72a
momen <momenk208@gmail.com>
2022-08-18 00:56:27 +0200

    Added keys and alts for images

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,7 +2,9 @@ import React from "react";
 
 const ImageList = (props) => {
   const photos = props.images.map((image) => {
-    return <img src={image.urls.regular} />;
+    return (
+      <img alt={image.description} key={image.id} src={image.urls.regular} />
+    );
   });
   return <div>{photos}</div>;
 };
