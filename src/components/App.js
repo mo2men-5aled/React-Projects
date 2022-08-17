dd3c176d892bd026d985002858c9546a2a62653a
momen <momenk208@gmail.com>
2022-08-17 20:10:55 +0200

    request unsplash api using axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      headers: {
+        Authorization: "Client-ID 9wtXa9NBiR90IWJ8G-bbe_TPC_2CWLN6ELEYvhm06jU",
+      },
+      params: { query: term },
+    });
   }
 
   render() {
