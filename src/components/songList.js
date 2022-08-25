4cebb62957d4ae0d5150d77e83e8095877158913
momen <momenk208@gmail.com>
2022-08-25 03:34:35 +0200

    return songs object

--- src/components/songList.js
+++ src/components/songList.js
@@ -2,14 +2,25 @@ import React from "react";
 import { connect } from "react-redux";
 
 class SongList extends React.Component {
+  renderList() {
+    return this.props.songs.map((songs) => {
+      return (
+        <div className="item">
+          <div className="right floated content">
+            <button className="ui button">Select</button>
+          </div>
+          <div className="content">{songs.title}</div>
+        </div>
+      );
+    });
+  }
   render() {
-    return <div>List song</div>;
+    return <div>{this.renderList}</div>;
   }
 }
 
 const mapStateToProps = (state) => {
-  console.log(state);
-  return state;
+  return { songs: state.songs };
 };
 
 export default connect(mapStateToProps)(SongList);
