f427fd119f1be36a01781b29e719314aeb146146
momen <momenk208@gmail.com>
2022-08-29 23:10:39 +0200

    added api & applyMiddleware &PostsRedcer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,6 @@
-export const fetchPosts = () => {
-  return { type: "FETCH_POSTS" };
+import jsonplaceholder from "../APIs/JsonPlacHolder";
+
+export const fetchPosts = () => async (dispatch) => {
+  const response = await jsonplaceholder.get("/posts");
+  dispatch({ type: "FETCH_POSTS", payload: response });
 };
