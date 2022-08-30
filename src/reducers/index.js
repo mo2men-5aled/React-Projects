d1b06e09ffe2972e2989921764d9b1d3c4f83192
momen <momenk208@gmail.com>
2022-08-30 23:30:00 +0200

    user header added & fetch user action added &user header reducer added

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import PostsReducer from "./PostsReducer";
+import usersReducer from "./usersReducer";
 
 export default combineReducers({
   posts: PostsReducer,
+  users: usersReducer,
 });
