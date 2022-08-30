1af05b3f744c5b0114fab1275f6b75c3a7eb44ca
momen <momenk208@gmail.com>
2022-08-31 00:07:53 +0200

    install memoize

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -7,14 +7,14 @@ class UserHeader extends React.Component {
     this.props.fetchUser(this.props.USERID);
   }
   render() {
-    const user = this.props.users.find((user) => user.id === this.props.USERID);
+    const { user } = this.props;
     if (!user) {
       return null;
     }
     return <div className="header">{user.name}</div>;
   }
 }
-const mapStateToProps = (state) => {
-  return { users: state.users };
+const mapStateToProps = (state, ownProps) => {
+  return { user: state.users.find((user) => user.id === ownProps.USERID) };
 };
 export default connect(mapStateToProps, { fetchUser })(UserHeader);
