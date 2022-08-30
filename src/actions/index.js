513b2523c0bc30508211c81c7ee1f6ab071d66e5
momen <momenk208@gmail.com>
2022-08-31 01:43:33 +0200

    apply the second way of preventing data overfetching

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,35 @@
 import _ from "lodash";
 import jsonplaceholder from "../APIs/JsonPlacHolder";
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  //making an array containing user IDs
+  const userIds = _.uniq(_.map(getState().posts, "userId"));
+
+  //iterating over each element inside the array
+  userIds.forEach((id) => dispatch(fetchUser(id)));
+};
+
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonplaceholder.get("/posts");
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-export const fetchUser = (id) => (dispatch) => {
-  _fetchUser(id, dispatch);
-};
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async (dispatch) => {
   const response = await jsonplaceholder.get(`/users/${id}`);
 
   dispatch({ type: "FETCH_USER", payload: response.data });
-});
+};
+
+// the first way if preventing overfetching using memoization
+
+// export const fetchUser = (id) => (dispatch) => {
+//   _fetchUser(id, dispatch);
+// };
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonplaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// });
