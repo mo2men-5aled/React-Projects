f81eda4a4e8411dba78758d7c1f6ac3c6c429007
momen <momenk208@gmail.com>
2022-08-31 22:00:43 +0200

    testing React Router Dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import PageOne from "./PageOne";
+import PageTwo from "./PageTwo";
 
 const App = () => {
-  return <div>Hi there!!</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<PageOne />} />
+          <Route path="/pageTwo" element={<PageTwo />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
