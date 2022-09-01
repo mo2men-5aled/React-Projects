205e11f7292e03a47252477a3d51f320dd79d8cb
momen <momenk208@gmail.com>
2022-09-01 16:12:39 +0200

    Routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,36 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
+const PageOne = () => {
+  return (
+    <div>
+      hohohohhohohoho
+      <br />
+      <Link to="/pagetwo">jump</Link>
+    </div>
+  );
+};
+
+const PageTwo = () => {
+  return (
+    <div>
+      hahahahahahahaha
+      <br />
+      <Link to="/">go back</Link>
+    </div>
+  );
+};
 const App = () => {
-  return <div>Hi there!!</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<PageOne />} />
+          <Route path="/pagetwo" element={<PageTwo />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
