8ae04d9df40cf5d1a364eccd50cba1a62cc29517
momen <momenk208@gmail.com>
2022-09-04 19:40:59 +0200

    add a stream list to the main page Stream list

--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -1,6 +1,11 @@
 import _ from "lodash";
+import streams from "../apis/streams";
 export default (state = {}, action) => {
   switch (action.type) {
+    case "FETCH_STREAMS":
+      //mapKeys is a function from lodash library that converts array to an objects
+      return { ...state, ..._.mapKeys(streams, "id") };
+
     case "FETCH_STREAM":
       return { ...state, [action.payload.id]: action.payload };
     case "CREATE_STREAM":
