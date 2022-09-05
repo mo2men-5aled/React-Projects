2c3b057e16ad9da42f83c7121e34642b7a1f1157
momen <momenk208@gmail.com>
2022-09-05 20:26:59 +0200

    added a modal in index.html

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,7 +1,30 @@
 import React from "react";
+import Modal from "../modal";
+//import { connect } from "react-redux";
+//import { deleteStream } from "../../actions";
+
+// class StreamDelete extends React.Component {
+//   componentDidMount() {
+//     this.props.deleteStream(this.props.match.params.id);
+//   }
+//   render() {
+//     return <div></div>;
+//   }
+// }
+// const mapStateToProps = (state, ownProps) => {
+//   return {
+//     stream: state.streams[ownProps.match.params.id],
+//   };
+// };
+
+// export default connect(mapStateToProps, { deleteStream })(StreamDelete);
 
 const StreamDelete = () => {
-  return <div>StreamDelete</div>;
+  return (
+    <div>
+      streamDelete
+      <Modal />
+    </div>
+  );
 };
-
 export default StreamDelete;
