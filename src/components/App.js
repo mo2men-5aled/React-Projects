23a9ac80f74e6c01fa8a32abb50a02f5556bef15
momen <momenk208@gmail.com>
2022-09-06 20:45:18 +0200

    form components (input & Button)

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,34 @@
 import React from "react";
+import UserCreate from "./UserCreate";
 
-const App = () => {
-  return <div>Hi there!!</div>;
-};
+class App extends React.Component {
+  state = { language: "english" };
+
+  onLanguageChange = (language) => {
+    this.setState({ language: language });
+  };
+  render() {
+    return (
+      <div className="ui container">
+        <div>
+          Select a Language:
+          <i
+            className="flag us"
+            onClick={() => {
+              this.onLanguageChange("english");
+            }}
+          />
+          <i
+            className="flag nl"
+            onClick={() => {
+              this.onLanguageChange("dutch");
+            }}
+          />
+        </div>
+        <UserCreate />
+      </div>
+    );
+  }
+}
 
 export default App;
