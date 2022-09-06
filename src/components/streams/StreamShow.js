bb8e3041579fc0138ff62d7f1105a497bc670dbd
momen <momenk208@gmail.com>
2022-09-06 18:47:24 +0200

    video stream shows in the stream show page

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,10 +1,41 @@
 import React from "react";
 import { connect } from "react-redux";
 import { fetchStream } from "../../actions";
+import flv from "flv.js";
 
 class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.videoRef = React.createRef();
+  }
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
+    this.buildPlayer();
+  }
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  //to make the player stop taking the data from the streaming video after closing the window or back to the main
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return <div>Loading ...</div>;
+    }
+
+    this.player = flv.createPlayer({
+      type: "flv",
+
+      //this.props.match.params.id) this is the stream id
+      // the stream id will be the stream name
+      url: `http://localhost:8000/live/${this.props.match.params.id}.flv`,
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
   }
   render() {
     if (!this.props.stream) {
@@ -12,6 +43,7 @@ class StreamShow extends React.Component {
     }
     return (
       <div>
+        <video ref={this.videoRef} style={{ width: "100%" }} controls />
         <h1>{this.props.stream.title}</h1>
         <h5>{this.props.stream.description}</h5>
       </div>
