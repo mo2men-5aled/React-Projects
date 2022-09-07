b0c030260c055453f60c86f34f3f538927ff6122
momen <momenk208@gmail.com>
2022-09-07 18:38:19 +0200

    added app components & state

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,30 @@
 import React from "react";
 
-const App = () => {
-  return <div>Hi there!!</div>;
-};
+class App extends React.Component {
+  state = { resource: "posts" };
+  render() {
+    return (
+      <div>
+        <div>
+          <button
+            onClick={() => {
+              this.setState({ resource: "Posts" });
+            }}
+          >
+            Posts
+          </button>
+          <button
+            onClick={() => {
+              this.setState({ resource: "Todos" });
+            }}
+          >
+            Todos
+          </button>
+        </div>
+        {this.state.resource}
+      </div>
+    );
+  }
+}
 
 export default App;
