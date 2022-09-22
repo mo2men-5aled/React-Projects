7c9b3a82ac66bae12a42dd094d7d9a353d272924
momen <momenk208@gmail.com>
2022-09-22 22:55:40 +0200

    install dependencies

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   return <div>Hi there!!</div>;
