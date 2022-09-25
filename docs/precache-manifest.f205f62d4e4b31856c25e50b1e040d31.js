152216529097f641965f87bc099118159d25b56f
momen <momenk208@gmail.com>
2022-09-26 00:30:03 +0200

    update build folder

---
+++ docs/precache-manifest.f205f62d4e4b31856c25e50b1e040d31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ea946ea1ba7e5ec856f",
+    "url": "/static/css/main.b87d7a6d.chunk.css"
+  },
+  {
+    "revision": "5ea946ea1ba7e5ec856f",
+    "url": "/static/js/main.5ea946ea.chunk.js"
+  },
+  {
+    "revision": "c1e1f7bc27a4f7888a7b",
+    "url": "/static/js/1.c1e1f7bc.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bbfc1765bb6282572f8def3ae96bb7f9",
+    "url": "/index.html"
+  }
+];
