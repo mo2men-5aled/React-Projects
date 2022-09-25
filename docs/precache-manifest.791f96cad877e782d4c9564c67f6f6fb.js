13afb56c06e10fc438b20b781d0c594a99cc9485
momen <momenk208@gmail.com>
2022-09-26 00:19:50 +0200

    create build folder

---
+++ docs/precache-manifest.791f96cad877e782d4c9564c67f6f6fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c7fe748e86689be9e5d",
+    "url": "/static/css/main.963f876d.chunk.css"
+  },
+  {
+    "revision": "9c7fe748e86689be9e5d",
+    "url": "/static/js/main.9c7fe748.chunk.js"
+  },
+  {
+    "revision": "9d2f92f9d8713096d497",
+    "url": "/static/js/1.9d2f92f9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1b4d3676be292eee07aa742221adbedd",
+    "url": "/index.html"
+  }
+];
